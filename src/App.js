30c5f9664715cd5fe9bcc965ffa634b443da762e
Santi Nuñez <tau150@hotmail.com>
2018-02-06 18:46:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+      <h1>WELCOME</h1>
+          {/* <Switch> */}
+            {/* <Route path="/" exact component={...} /> */}
+          {/* </Switch> */}
+      </div>
+    );
+  }
+}
+
+export default App;
